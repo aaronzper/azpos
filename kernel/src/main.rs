ad81464d0be8de5b515bf65a44d829f89d133a2f
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-05-12 18:08:14 -0400

    feat: Map physical memory into VM and print memory info

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-
-use bootloader_api::{info::Optional, BootInfo};
+use bootloader_api::{config::Mapping, info::Optional, BootInfo, BootloaderConfig};
 use devices::fb::{FbTerminal, Framebuffer};
 use logger::set_logger;
 
@@ -11,7 +9,12 @@ mod panic;
 mod logger;
 mod devices;
 
-bootloader_api::entry_point!(kmain);
+const BOOTCONFIG: BootloaderConfig = {
+    let mut conf = BootloaderConfig::new_default();
+    conf.mappings.physical_memory = Some(Mapping::Dynamic);
+    conf
+};
+bootloader_api::entry_point!(kmain, config = &BOOTCONFIG);
 
 fn kmain(boot_info: &'static mut BootInfo) -> ! {
     let fb_raw = match &mut boot_info.framebuffer {
@@ -19,12 +22,23 @@ fn kmain(boot_info: &'static mut BootInfo) -> ! {
         Optional::None => panic!("No framebuffer!"),
     };
 
+    let fb_addr = fb_raw.buffer().as_ptr();
+
     let fb = Framebuffer::new(fb_raw);
     let t = FbTerminal::new(fb);
     set_logger(t);
 
     println!("Hello world!");
-    println!("{:?}", boot_info.memory_regions);
+    println!("Kernel Start:\t{:#X}", boot_info.kernel_image_offset);
+    println!("Kernel End:\t{:#X}", boot_info.kernel_image_offset + boot_info.kernel_len);
+    println!("Phys Mapping:\t{:#X}", boot_info.physical_memory_offset.take().unwrap());
+    println!("Fb Mapping:\t{:?}", fb_addr);
+    println!("Memory Regions:\n{:?}", boot_info.memory_regions);
+
+    for i in 0..=u8::MAX as u16 * 3 {
+        print!("R");
+        for _ in 0..=u8::MAX as u32 * 2000 {}
+    }
 
     panic!("End of kmain");
 }
