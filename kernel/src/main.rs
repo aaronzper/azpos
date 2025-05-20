7acc974e435efa54c3899267cec7a8b7303787f4
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-05-20 16:57:44 -0400

    setup: Interrupt subsystem

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,11 +7,12 @@ extern crate alloc;
 use alloc::{boxed::Box, vec::Vec};
 use bootloader_api::{config::Mapping, info::Optional, BootInfo, BootloaderConfig};
 use devices::fb::{FbTerminal, Framebuffer};
+use interrupts::init_interrupts;
 use logger::set_logger;
 use memory::{get_heap_size, init_memory, KERNEL_START_ADDR};
 
 #[macro_use]
-/// Global Kernel logger
+/// Global kernel logger
 mod logger;
 /// Kernel panic functionality
 mod panic;
@@ -19,6 +20,8 @@ mod panic;
 mod devices;
 /// Memory subsystem (paging, the heap, etc)
 mod memory;
+/// CPU interrupt subsystem (faults, hardware interrupts, & syscalls)
+mod interrupts;
 
 const BOOTCONFIG: BootloaderConfig = {
     let mut conf = BootloaderConfig::new_default();
@@ -30,36 +33,20 @@ bootloader_api::entry_point!(kmain, config = &BOOTCONFIG);
 
 /// Initializes the kernel and its subsystems
 fn kmain(boot_info: &'static mut BootInfo) -> ! {
+    let pmap = boot_info.physical_memory_offset.take().unwrap();
+    init_memory(pmap, &boot_info.memory_regions);
+
     let fb = match &mut boot_info.framebuffer {
         Optional::Some(x) => x,
         Optional::None => panic!("No framebuffer!"),
     };
     let fb_info = fb.info().clone();
     let fb_buf = fb.buffer_mut();
-
-    let pmap = boot_info.physical_memory_offset.take().unwrap();
-    init_memory(pmap, &boot_info.memory_regions);
-
     let fb = Framebuffer::new(fb_buf, fb_info);
     let t = FbTerminal::new(fb);
     set_logger(t);
-    println!("Hello world!");
 
-    let mut v: Vec<Box<u128>> = Vec::new();
-    for i in 0..5000 {
-        let b = Box::new(i);
-        v.push(b);
-    }
-
-    println!("\nBoxes:");
-    for b in &v {
-        print!("{}\t", b);
-    }
-    println!("");
-
-    println!("Before drop heap size: {} kb", get_heap_size() / 1024);
-    drop(v);
-    println!("After drop heap size:  {} kb", get_heap_size() / 1024);
+    init_interrupts();
 
     panic!("End of kmain");
 }
