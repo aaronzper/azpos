a4b993699d72d145df1982064e88545355645d1a
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-05-22 01:06:53 -0400

    feat: CLI flags for running with UEFI or GDB so I dont need to hardcode it every time lol

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
+    let args: String = std::env::args().collect();
+
     // read env variables that were set in build script
     let uefi_path = env!("UEFI_PATH");
     let bios_path = env!("BIOS_PATH");
     
     // choose whether to start the UEFI or BIOS image
-    let uefi = false;
+    let uefi = args.contains("-uefi");
 
     // whether to run QEMU with GDB
-    let gdb = false;
+    let gdb = args.contains("-gdb");
 
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
     cmd.arg("-m").arg("2G");
