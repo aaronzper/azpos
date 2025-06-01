0000f02fcdf0b8b1355408d26fbba2305c3887f9
Aaron P <19561067+aaronzper@users.noreply.github.com>
2025-06-01 01:56:44 -0400

    cleanup: Finish MMIO allocator (I think i accidentally committed it earlier...whoops)

--- kernel/src/memory/mmio.rs
+++ kernel/src/memory/mmio.rs
@@ -1,9 +1,6 @@
-use core::alloc::Layout;
-use alloc::alloc::alloc;
-use x86_64::{structures::paging::{frame::PhysFrameRange, FrameDeallocator, Mapper, Page, PageTableFlags}, PhysAddr, VirtAddr};
-use crate::memory::{paging::current_pt, PAGE_ALLOCATOR};
-
-use super::{paging::SizedPage, resolve_phys_addr, PAGE_SIZE};
+use x86_64::structures::paging::frame::PhysFrameRange;
+use crate::memory::PAGE_ALLOCATOR;
+use super::{resolve_phys_addr, PAGE_SIZE};
 
 /// Allocates a block of physical memory that:
 /// - Is page-aligned, and padded until the start of the next page after the
@@ -19,25 +16,13 @@ use super::{paging::SizedPage, resolve_phys_addr, PAGE_SIZE};
 pub unsafe fn alloc_mmio_block<T>(len: usize) -> Option<(*mut T, PhysFrameRange)> {
     let n_pages = (len + PAGE_SIZE as usize - 1) / PAGE_SIZE as usize;
 
-    // Remap the allocation to contiguous physical frames w/ cache disabled
     let mut lock = PAGE_ALLOCATOR.lock();
     let page_alloc = lock.as_mut().unwrap();
-    let mut pt = current_pt();
 
     let frames = page_alloc.alloc_range(n_pages)?;
     let start_pa = frames.start.start_address();
     let start_va = resolve_phys_addr(start_pa).unwrap();
-    
-    let flags = PageTableFlags::PRESENT 
-        | PageTableFlags::WRITABLE 
-        | PageTableFlags::NO_CACHE;
-    let mut va = start_va;
-    for _ in frames {
-        let page = SizedPage::from_start_address(va).unwrap();
-        let flusher = unsafe { pt.update_flags(page, flags).unwrap() };
-        flusher.flush();
-        va += PAGE_SIZE;
-    }
+    let ptr = start_va.as_mut_ptr();
 
-    Some((start_va.as_mut_ptr(), frames))
+    Some((ptr, frames))
 }
